d5551dba59a066c7d6972907ca5a3de0bf37cf8c
Richard Frank <rich@quantopian.com>
2020-03-09 10:24:06 -0400

    Revert "replaced lru-dict with functools"
    This reverts commit e040165c836998315b738f902f1cbf9cb9d4c6a4.

--- zipline/data/minute_bars.py
+++ zipline/data/minute_bars.py
@@ -12,16 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from abc import ABCMeta, abstractmethod
-try:
-    from functools32 import lru_cache
-except ImportError:
-    from functools import lru_cache
 import json
 import os
 from glob import glob
 from os.path import join
 from textwrap import dedent
 
+from lru import LRU
 import bcolz
 from bcolz import ctable
 from intervaltree import IntervalTree
@@ -949,7 +946,7 @@ class BcolzMinuteBarReader(MinuteBarReader):
         self._minutes_per_day = metadata.minutes_per_day
 
         self._carrays = {
-            field: lru_cache(sid_cache_sizes[field])(lambda sid: sid)
+            field: LRU(sid_cache_sizes[field])
             for field in self.FIELDS
         }
 
@@ -1073,7 +1070,7 @@ class BcolzMinuteBarReader(MinuteBarReader):
         sid = int(sid)
 
         try:
-            carray = self._carrays[field](sid)
+            carray = self._carrays[field][sid]
         except KeyError:
             try:
                 carray = self._carrays[field][sid] = bcolz.carray(
